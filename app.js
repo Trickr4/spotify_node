a85dc5305193a171034864dba507ed9e93071a5e
Nettsuka <jonathanchhou@Outlook.com>
2021-09-03 15:32:38 -0700

    change routing

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.set('views', path.join(__dirname + '/dist/client'));
 if (process.env.NODE_ENV === "production") {
     app.use('/',express.static(path.join(__dirname + '/dist/client')));
 
-    //app.get('/*', function(req,res) {
-   // res.sendFile( path.join(__dirname+'/dist/client/index.html'));});
+    app.get('/*', function(req,res) {
+    res.sendFile( path.join(__dirname+'/dist/client/index.html'));});
 
 }
 
