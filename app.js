fe2e10c51c4976b0d8fbe13a3a0b420b698dc466
Nettsuka <jonathanchhou@Outlook.com>
2021-09-02 22:18:54 -0700

    change angular

--- app.js
+++ app.js
@@ -46,12 +46,13 @@ if (process.env.NODE_ENV === "production") {
 
 
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     
 res.sendFile( path.join(__dirname+'./dist/client/index.html')
  );
 });
 app.use(express.static(__dirname + '/client'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
