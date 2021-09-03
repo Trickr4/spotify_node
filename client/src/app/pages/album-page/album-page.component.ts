4cbd28d3a047c91a3f0b34f03cc6c2ecb2f0311c
Nettsuka <jonathanchhou@Outlook.com>
2021-09-02 20:05:11 -0700

    angular changes

---
+++ client/src/app/pages/album-page/album-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArtistData } from '../../data/artist-data';
+import { TrackData } from '../../data/track-data';
+import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from '../../services/spotify.service';
+
+@Component({
+  selector: 'app-album-page',
+  templateUrl: './album-page.component.html',
+  styleUrls: ['./album-page.component.css']
+})
+export class AlbumPageComponent implements OnInit {
+	albumId:string;
+	album:AlbumData;
+	tracks:TrackData[];
+
+
+  constructor(private route: ActivatedRoute, private spotifyService:SpotifyService) { }
+
+  ngOnInit() {
+  	this.albumId = this.route.snapshot.paramMap.get('id');
+    //TODO: inject spotifyService and use it to get the album data and the tracks for the album
+    
+    this.spotifyService.getAlbum(this.albumId).then(data => {
+      this.albumId = data.id;
+      data.artists[0].url = data.artists[0].url.replace("https://open.spotify.com", "");
+      this.album = data;
+    });
+    this.spotifyService.getTracksForAlbum(this.albumId).then(data => {
+      data.forEach((item) => {
+        item.album = this.album;
+        });
+      this.tracks = data;
+    });
+  }
+
+}
