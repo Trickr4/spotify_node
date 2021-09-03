0e757598dbf67c7a7b807edc2a09ddc3f08cf87a
Nettsuka <jonathanchhou@Outlook.com>
2021-09-02 21:21:27 -0700

    change angular

--- client/server.js
+++ client/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/client'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/client/index.html'));
+res.sendFile(path.join(__dirname+'/dist'));
 });
 
 // Start the app by listening on the default Heroku port
