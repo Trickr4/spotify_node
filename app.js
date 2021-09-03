2bb2f0e2313283d27bce0aefe5ca7dc034d273d6
Nettsuka <jonathanchhou@Outlook.com>
2021-09-02 20:23:10 -0700

    change angular

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(__dirname + 'client/dist'));
 app.get('/*', function(req,res) {
     
 res.sendFile( path.resolve( 
-  path.join(__dirname+'client/dist/index.html')
+  path.join(__dirname+'client/')
 ) );
 });
 
