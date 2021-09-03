b66d78a0258918474d5bfddbe545289c30fba289
Nettsuka <jonathanchhou@Outlook.com>
2021-09-03 02:07:19 -0700

    change angular

--- app.js
+++ app.js
@@ -42,13 +42,13 @@ app.use('/', indexRouter);
 
 // Serve only the static files form the dist directory
 //if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(process.env.PWD + '/client')));
+  app.use(express.static(path.join(process.env.PWD + '/dist/client')));
 //}
 
 
 
 app.get('*', function(req,res) {
-    res.sendFile( path.join(process.env.PWD+'/client/src/index.html')
+    res.sendFile( path.join(process.env.PWD+'/dist/client/index.html')
  );
 });
 
