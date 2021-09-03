6992c95cff6c23eea41265eb91023583526a0dce
Nettsuka <jonathanchhou@Outlook.com>
2021-09-02 21:30:17 -0700

    change angular

--- app.js
+++ app.js
@@ -38,6 +38,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + './dist/client''));
+
+app.get('*', function(req,res) {
+    
+res.sendFile( path.resolve( 
+        __dirname,
+        "client",
+        "dist",
+        "index.html"
+) );
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
